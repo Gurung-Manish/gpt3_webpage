d64d199a9a95dc0e2611d16c0cf3391ed8b9464f
Gurung-Manish <manishgrg496@gmail.com>
2024-08-21 16:11:09 +0100

    new assets file, new css and stuff

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
 import "./navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__nabar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo"></img>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
