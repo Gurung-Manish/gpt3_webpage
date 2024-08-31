6aeae483ee0a3cca9d1b5c928be1e10c57a9c48b
Gurung-Manish <manishgrg496@gmail.com>
2024-08-31 11:07:32 +0100

    feature css

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -46,9 +46,11 @@ const featuresData = [
 const Feature = ({ title, text }) => {
   return (
     <div className="gpt3__features-container__feature ">
-      <div className="gpt3__features-container__feature-bar" />
-      <div className="gpt3__features-container__feature-title"></div>
-      <h1>{title}</h1>
+      <div className="gpt3__features-container__feature-title">
+        <div className="gpt3__features-container__feature-bar" />
+        <h1>{title}</h1>
+      </div>
+
       <div className="gpt3__features-container_feature-text">{text}</div>
     </div>
   );
